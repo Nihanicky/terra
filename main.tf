6c5489fbd934da7a4f1fb7c75a4830673daf54df
Nihanicky <138659994+Nihanicky@users.noreply.github.com>
2025-08-30 18:53:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0a4723d2c63bae7c3" # valid AMI for ap-south-1
   instance_type = "t2.micro"
-  subnet_id     = "subnet-123abc456" # replace with a real subnet
+  subnet_id     = "subnet-041327ee469120fb1" # replace with a real subnet
   tags = {
     Name = "MyTerraformEC2"
   }
